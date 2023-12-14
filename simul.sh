367044f7f7d8508d5839ccbda7ed0dfdcc104922
Cesar Silva <153388220+cesarensino@users.noreply.github.com>
2023-12-14 10:40:53 -0300

    Add files via upload

---
+++ simul.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#aplique chmod +x para todos os scripts
+
+# Chama o script1.sh
+./sc1-hosts.sh
+
+sleep 10
+
+# Chama o script2.sh
+./sc2-novoforum.sh
+sleep 10
+
+# Chama o script3.sh
+./sc3-participantes.sh
+sleep 10
+
+# Chama o script4.sh
+./sc4-conversas1.sh
+sleep 10 
+
+./sc5-conversas2.sh
+sleep 10
+
+./sc6-conversas6.sh
+sleep 10
+
+./sc7-conversas7.sh
+sleep 10
+
+./sc8-conversas8.sh
+sleep 10
+
+./sc9-conversas9.sh
+sleep 10
+
+./sc10-final
+
+
